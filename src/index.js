ecb0a23a1469e4b03d40875e754dfcd5867f6b8e
Jose Angel Garcia Nunez <joseagnunez@gmail.com>
2023-08-11 07:55:05 -0700

    Add and export createNavigation function Add and export createNavigation function for dynamic navigation tab generation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './styles.css';
+import { createNavigation } from './navigation';
 
 const container = document.createElement('div');
 container.id = 'container';
 document.body.appendChild(container);
+
+createNavigation();
---
+++ src/navigation.js
@@ -0,0 +1,24 @@
+export function createNavigation(){
+    const container = document.getElementById('container');
+
+    const navigationHeader = document.createElement('header');
+    navigationHeader.id = 'navigationHeader';
+    container.appendChild(navigationHeader);
+
+    const navigationUl = document.createElement('ul');
+    navigationUl.id = 'navigationUl';
+    navigationHeader.appendChild(navigationUl);
+
+    const navTabs = ['Inbox', `Today's tasks`, 'Upcoming tasks'];
+
+    navTabs.forEach(navTab =>{
+        const li = document.createElement('li');
+        const navTabId = navTab.replace(/[^a-zA-Z ]/g, '').charAt(0).toLowerCase() + navTab.slice(1).replace(/[^a-zA-Z ]/g, '').replace(/ (\w)/g, function(match, p1) {
+        return p1.toUpperCase();
+        });
+        li.id = `${navTabId}Tab`;
+        li.className = 'navTabs';
+        navigationUl.appendChild(li);
+        li.textContent = `${navTab}`;
+    });
+};
